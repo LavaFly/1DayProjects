e2657d6925e55f0b590c68ab950b4429b5a01196
LavaFly <lucakannenberg@gmail.com>
2020-08-05 15:38:38 +0200

    Done

--- notes.lua
+++ notes.lua
@@ -1,17 +1,28 @@
+#!/usr/bin/lua5.3
 --setup
 note_lines = {}
 tone_names = {'c','d','e','f','g','a','h','c','d','e','f','g','a','h'}
 random_notes = {}
 notes = ""
-num_notes = 10
+length = 82
+num_notes = 20
+
+
+--the rating limits the spreading of the notes
+rating = 2
+current_value = 7
+
+
 math.randomseed(os.time())
 math.random();math.random()
-while(1) do
-  --creating the lines
+
+
+--initializing the board
+function init_board()
   for i = 1,14,1
   do
     note_lines[i] = {}
-    for j = 1,42,1
+    for j = 1,length,1
     do
       if(i % 2 == 0 and i <= 12 and i >= 3)
       then
@@ -19,47 +30,80 @@ while(1) do
       else
         note_lines[i][j] = ' '
       end
-    
     end
   end
+end
 
 
---choosing notes
-  for i = 1,num_notes,1
+--function to round numbers
+function round(to_round)
+  return math.floor(to_round + 0.5)
+end
+
+
+--function to regulate the spreading of the notes
+function choose_next_note()
+  current_value = ( round( math.random() ) == 0 ) and 
+            ( current_value + math.random(0, rating) ) or
+            ( current_value - math.random(0, rating) )
+  return current_value
+end
+
+
+--function to if the value is valid
+function not_zero(value)
+  if(value % 14 == 0)
+  then
+    return 1
+  else
+    return value % 14
+  end
+end  
+
+
+--main
+while(1) do
+  init_board()
+  
+  --choosing notes
+  for i = 1,num_notes-1,1
   do
-    random_notes[i] = math.random(1,14)
-  
-    note_lines[15 - random_notes[i] ][ ( i * 2 ) - 1] = 'X'
-  
-    notes = notes..tone_names[tonumber(random_notes[i])]
+    random_notes[i] = not_zero(choose_next_note())   
+    note_lines[15 - random_notes[i] ][ ( i * 4 ) - 1 ] = 'X'
+    notes = notes .. tone_names[tonumber(random_notes[i])]
   end
 
---printing everything
+  --printing everything
   for i = 1,14,1
   do
-    io.write(tone_names[15 - i]..'|')
-    for j = 1,42,1
+    if( rating >= 10 )
+    then
+      io.write(" |")
+    else 
+      io.write(tone_names[15 - i]..'|')
+    end
+    
+    for j = 1,length,1
     do
       io.write(note_lines[i][j])
     end
-    io.write('|')
-    print()
+    io.write("|\n")
   end
-  io.write(" |1   5    10   15   20   25   30   35   40 |")
-  print()
-  io.write("  ")
-  print(notes)
-  print()
-
+  
+  io.write("\n->:")
   input = io.read()
+  
   if(input == "exit")
   then
     break
   elseif (input == notes)
   then
-    print("yes")
+    io.write("correct!\n your rating has improved\n")
+    if(rating < 15) then rating = rating + 1 end
   else
-    print("no\n")
-    print(notes)
+    io.write("false!\n your rating has decreased\n")
+    io.write("correct notes: "..notes)
+    if(rating >= 0) then rating = rating - 1 end
   end
+  notes = ""
 end
